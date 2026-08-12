ce73e479d685868637483b7e7dd3082a9814e0bb
Yosssef9 <147534893+Yosssef9@users.noreply.github.com>
2026-08-12 11:51:08 +0300

    chore: remove unused legacy backend files

--- server/services/budgetTimeline.service.js
+++
@@ -1,5 +0,0 @@
-import { getBudgetTimelineRepo } from "../repositories/budgetTimeline.repository.js";
-
-export async function getBudgetTimelineService(budgetId) {
-  return await getBudgetTimelineRepo(budgetId);
-}
